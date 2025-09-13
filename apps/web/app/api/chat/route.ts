9f0cbc2299379083c8727e147b3efa97c01913e4
Sahil Tiwaskar <sahil@Sahils-MacBook-Air.local>
2025-09-13 21:05:36 +0530

    add web search

--- apps/web/app/api/chat/route.ts
+++ apps/web/app/api/chat/route.ts
@@ -1,5 +1,6 @@
 import { openrouter } from '@/lib/openrouter';
-import { streamText, UIMessage, convertToModelMessages } from 'ai';
+import { streamText, UIMessage, convertToModelMessages, stepCountIs } from 'ai';
+import tools from '@/lib/tools';
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -10,6 +11,8 @@ export async function POST(req: Request) {
   const result = streamText({
     model: openrouter('nvidia/nemotron-nano-9b-v2:free'),
     messages: convertToModelMessages(messages),
+    tools: tools,
+    stopWhen: stepCountIs(5),
   });
 
   return result.toUIMessageStreamResponse();
