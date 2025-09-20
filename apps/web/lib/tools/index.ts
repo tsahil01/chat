38ea9bddf134c2826b4c5410822b665f35b2d858
sahiltworks@gmail.com <sahil@Sahils-MacBook-Air.local>
2025-09-20 23:45:48 +0530

    add gh tools

--- apps/web/lib/tools/index.ts
+++ apps/web/lib/tools/index.ts
@@ -1,12 +1,44 @@
 import { ToolSet } from "ai";
 import { exaWebSearch } from "./web-search";
-import { createCalendarEvent, listCalendarEvents, checkCalendarAvailability } from "./google";
+import { 
+  createCalendarEvent, 
+  listCalendarEvents, 
+  checkCalendarAvailability 
+} from "./google";
+
+import { 
+  listGitHubRepos, 
+  createGitHubRepo, 
+  getGitHubRepoInfo,
+  listGitHubIssues, 
+  createGitHubIssue, 
+  getGitHubIssue,
+  getGitHubUserInfo,
+  listGitHubFollowers
+} from "./github";
 
 const tools: ToolSet = {
+  // Web search
   exaWebSearch,
+  
+  // Google Calendar
   createCalendarEvent,
   listCalendarEvents,
   checkCalendarAvailability,
+  
+  // GitHub Repositories
+  listGitHubRepos,
+  createGitHubRepo,
+  getGitHubRepoInfo,
+  
+  // GitHub Issues
+  listGitHubIssues,
+  createGitHubIssue,
+  getGitHubIssue,
+  
+  // GitHub Users
+  getGitHubUserInfo,
+  listGitHubFollowers,
 };
 
 export default tools;
